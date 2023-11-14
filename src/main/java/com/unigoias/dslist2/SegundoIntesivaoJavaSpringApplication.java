0573de5bf9917419211cc66fe13e88675c5c43d7
Fabricio Freitas <fabricio.freitas.ad@gmail.com>
2023-11-14 13:50:45 -0300

    Projetc created

---
+++ src/main/java/com/unigoias/dslist2/SegundoIntesivaoJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.unigoias.dslist2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundoIntesivaoJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundoIntesivaoJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unigoias/dslist2/SegundoIntesivaoJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unigoias.dslist2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundoIntesivaoJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
