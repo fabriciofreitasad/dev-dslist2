94aaa616e90fad825f939597583e6811bae2415d
Fabricio Freitas <fabricio.freitas.ad@gmail.com>
2023-11-14 15:44:25 -0300

    seed

--- src/main/java/com/unigoias/dslist2/entities/Game.java
+++ src/main/java/com/unigoias/dslist2/entities/Game.java
@@ -21,29 +21,33 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String platform;
+	private String platforms;
 	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
-	private GameList gameList;
+
 
 	public Game() {
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platform, Double score, String imgUrl,
-			String shortDescription, String longDescription, GameList gameList) {
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
+			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
 		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
-		this.gameList = gameList;
+
 	}
 
 	public Long getId() {
@@ -78,12 +82,12 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatform(String platforms) {
+		this.platforms = platforms;
 	}
 
 	public Double getScore() {
@@ -118,13 +122,7 @@ public class Game {
 		this.longDescription = longDescription;
 	}
 
-	public GameList getGameList() {
-		return gameList;
-	}
-
-	public void setGameList(GameList gameList) {
-		this.gameList = gameList;
-	}
+	
 
 	@Override
 	public int hashCode() {
